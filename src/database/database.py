8f00852601d5a042b5f1a72ab5037f66531daac8
raejvich <gustav.6.johansson@hotmail.com>
2025-02-14 14:16:54 +0100

    log files and db close connection

--- src/database/database.py
+++ src/database/database.py
@@ -1,6 +1,10 @@
 import psycopg2
 import logging
 from src.log_config import db_logger
+from psycopg2 import sql
+from psycopg2.extras import DictCursor
+from typing import List, Tuple, Any, Optional
+
 
 # Configure logging
 logging.basicConfig(
@@ -38,10 +42,23 @@ class DataBaseConnect:
                 host=self.host,
                 port=self.port,
             )
+            # Enables dictionary-like query results
+            self.cursor = self.conn.cursor(cursor_factory=DictCursor)
             db_logger.info("Connecter to database successfully")
         except psycopg2.Error as e:
             db_logger.error(f"Error connecting to database: {e}")
 
+    def close(self):
+        """
+        Closes the database connection.
+        """
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
+            db_logger.info("Database connection closed.")
+
 
 admin = DataBaseConnect("itstime_db", "admin", "pw_admin")
 admin.connect()
+admin.close()
