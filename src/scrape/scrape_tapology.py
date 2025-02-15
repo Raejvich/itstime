64474987f66de5b5ea66171eee3c3fc7af0e9bba
raejvich <gustav.6.johansson@hotmail.com>
2025-02-15 12:25:50 +0100

    create data storage class

--- src/scrape/scrape_tapology.py
+++ src/scrape/scrape_tapology.py
@@ -8,8 +8,12 @@ class WebScraper:
     def __init__(self):
         self.url = "https://www.tapology.com/fightcenter?group=ufc"
         self.event_data = []
+        self.structured_event_data = []
         self.event_time = None
         self.event_name = None
+        self.event_location = None
+        self.fights = []
+        self.fighters = []
 
     def request_html(self):
         """
@@ -52,9 +56,6 @@ class WebScraper:
             for span_tag in span_tags:
                 event_raw.append(span_tag.get_text(strip=True))
 
-            # Store all collected event times, if any
-            self.event_times = event_raw if event_raw else "N/A"
-
             self.event_data.append(event_raw)
 
     def convert_date(self):
@@ -73,8 +74,77 @@ class WebScraper:
             date_obj = date_obj.replace(year=current_year)
         self.event_time = date_obj
 
+    def get_fights(self, event):
+        self.fights = []
+        self.fighters = []
+        for entry in event:
+            if "vs." in entry:
+                entry = entry.split("vs.")  # Split at 'vs'
+                entry = [fighter.strip() for fighter in entry]
+                self.fights.append(entry)
+        self.fighters = [fighter for fight in self.fights for fighter in fight]
+
+    def structure_data(self):
+        """
+        Example structure:
+        [event_obj_1, event_obj_2 ]
+            "event_name" : "UFC 312"
+            "event_date" : datetime(2020-01-01 18:00)
+            "event_location" : "Mexico City"
+            "event_fights" : ["fighter 1 vs. figter 2", "fighter 3 vs. fighter 4", etc.]
+            "event_fighters" : ["fighter 1", "fighter 2", "fighter 3", etc]
+        """
+        for event in self.event_data:
+            # name is stored in self.event_name or self.event_data[-][0]
+            self.event_name = event[0]
+            # date is stored in self.event_data[-][1], self.convert_date() to get correct format
+            self.event_time = event[1]
+            self.convert_date()
+            # location stored self.event_data[-][2]
+            self.event_location = event[2]
+            # fights use vs. as identifier, make function for cleaning
+            self.get_fights(event[1:])
+            self.structured_event_data.append(
+                Event(
+                    self.event_name,
+                    self.event_time,
+                    self.event_location,
+                    self.fights,
+                    self.fighters,
+                )
+            )
+
+
+class Event:
+    def __init__(
+        self,
+        event_name: str,
+        event_time: datetime,
+        event_location: str,
+        event_fights: list,
+        event_fighters: list,
+    ):
+        self.event_name = event_name
+        self.event_time = event_time
+        self.event_location = event_location
+        self.event_fights = event_fights
+        self.event_fighters = event_fighters
+
+    def __repr__(self):
+        return (
+            f"Event(\n"
+            f"  event_name={self.event_name!r},\n"
+            f"  event_time={self.event_time!r},\n"
+            f"  event_location={self.event_location!r},\n"
+            f"  event_fights={self.event_fights!r},\n"
+            f"  event_fighters={self.event_fighters!r}\n"
+            f")"
+        )
+
 
 if __name__ == "__main__":
     fight_data = WebScraper()
     fight_data.get_event_data()
-    print(fight_data.event_data)
+    fight_data.structure_data()
+
+    print(fight_data.structured_event_data[-1])
