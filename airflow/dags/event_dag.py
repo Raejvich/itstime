a4a3c8efe90c1fbb4a65b72bfe9124316221c1d2
raejvich <gustav.6.johansson@hotmail.com>
2025-03-17 18:19:12 +0100

    compose and docker files

--- airflow/dags/event_dag.py
+++ airflow/dags/event_dag.py
@@ -46,11 +46,12 @@ def connect_db(task_instance):
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
+    DB_HOST = os.getenv("DB_HOST")
     admin = DataBaseConnect(
         DB_NAME,
         DB_USER,
         DB_PASSWORD,
-        "host.docker.internal",
+        DB_HOST,
     )
     # connect to DB
     admin.connect()
